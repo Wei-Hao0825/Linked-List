f9b8a8a14e0192b41c64f2bcddd0e4713e74b8ac
Wei-Hao0825 <54641968+Wei-Hao0825@users.noreply.github.com>
2021-02-08 18:53:39 +0800

    Add files via upload

---
+++ LinkedList.js
@@ -0,0 +1,117 @@
+class LinkedList {
+    constructor () {
+        this.head = null
+        this.tail = null
+    }
+
+    append(data) {  // Add element to the end of the list
+        const newNode = {value: data, next: null}
+
+        if (this.tail) {
+            this.tail.next = newNode // if the current list has a tail, set the current tail's next point to the new data
+        }
+
+        this.tail = newNode
+
+        if (!this.head) {
+            this.head = newNode // if the current head does not have a value, means the list is empty, add the new data to the head of the list.
+        }
+    }
+    prepend(data) {
+        const newNode = {value: data, next: null}
+
+        const curHead = this.head
+
+        this.head = newNode // set the new data to the head
+
+        if (curHead) {
+            this.head.next = curHead // if the list already has a head, set the new head's next data to the current head.
+        }
+    }
+
+    removeHead() {
+        const removedHead = this.head
+
+        if (!removedHead) {
+            return // if the head needs to remove does not exist, return to stop
+        }
+        this.head = removedHead.next // set the removed head's next data to be the new head.
+    }
+
+    delete(data) {
+        if (!this.head) {
+            return
+        }
+        while (this.head && this.head.value === data) {
+            this.head = this.head.next
+        }
+
+        let curNode = this.head
+        while (curNode.next) {
+            if (curNode.next.value === data) {
+                curNode.next = curNode.next.next
+            } else {
+                curNode = curNode.next
+            }
+        }
+
+        if (this.tail.value === data) {
+            this.tail = curNode
+        }
+    }
+
+    find(data) {
+        if (!this.head) {
+            return
+        }
+
+        let curNode = this.head
+
+        while (curNode) {
+            if (curNode.value === data) {
+                return curNode
+            }
+
+            curNode = curNode.next
+        }
+
+        return null
+    }
+
+    insertAfter(data, afterData) {
+        const nodeExist = this.find(afterData)
+
+        if (nodeExist) {
+            const newNode = {value: data, next: nodeExist.next}
+            nodeExist.next = newNode
+        }
+    }
+    
+    toArray() {
+        const elements = []
+        let curNode = this.head
+        while (curNode) {
+            elements.push(curNode)
+            curNode = curNode.next
+        }
+        return elements
+    }
+}
+
+const linkedlist = new LinkedList()
+
+linkedlist.append('aaa')
+linkedlist.append(12)
+linkedlist.append(true)
+linkedlist.append(123.5)
+linkedlist.append('bbb')
+linkedlist.prepend('ccc')
+
+
+console.log(linkedlist.toArray())
+
+console.log(linkedlist.find('ccc'))
+
+linkedlist.insertAfter('ddd', 'bbb')
+
+console.log(linkedlist.toArray())
